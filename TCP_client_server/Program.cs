3e06e949503b97f2fef262d61b2b75676493c66a
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2020-03-06 06:53:56 +0200

    Add files via upload

---
+++ TCP_client_server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCP_client_server
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            //Task.Run(() => Listen());
+
+            //Listen();
+            /*
+            // Console.ReadLine();
+            //Thread.Sleep(100000000);
+
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                //tcpclnt.Connect("127.0.0.1", 8001);
+                tcpclnt.Connect("104.28.11.45", 80);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
+
+                //String str = Console.ReadLine();
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                // Console.WriteLine("Tran'smitting.....");
+                //byte[] ba = asen.GetBytes("aaa");
+                FileStream f1 = new FileStream("test.txt", FileMode.Open, FileAccess.Read);
+                byte[] ba = new byte[257];
+                f1.Read(ba, 0, 257);
+                f1.Close();
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100000];
+                int k = stm.Read(bb, 0, 3000);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+            */
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TCP_client_server/Server.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCP_client_server
+{
+    class Server
+    {
+        public static void Listen()
+        {
+            while (true)
+            {
+                try
+                {
+                    IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+                    // use local m/c IP address, and 
+                    // use the same in the client
+
+                    /* Initializes the Listener */
+                    TcpListener myList = new TcpListener(ipAd, 8001);
+
+                    /* Start Listeneting at the specified port */
+                    myList.Start();
+
+                    Console.WriteLine("The server is running at port 8001...");
+                    Console.WriteLine("The local End point is  :" +
+                                      myList.LocalEndpoint);
+                    Console.WriteLine("Waiting for a connection.....");
+
+                    Socket s = myList.AcceptSocket();
+                    Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                    byte[] b = new byte[10000];
+                    int k = s.Receive(b);
+                    Console.WriteLine("Recieved...");
+                    for (int i = 0; i < k; i++)
+                        Console.Write(Convert.ToChar(b[i]));
+
+                    // write the byte array into a new file
+                    byte[] data = File.ReadAllBytes("response.txt");
+                    s.Send(data);
+
+                    Console.WriteLine("\nSent Acknowledgement");
+                    /* clean up */
+                    s.Close();
+                    myList.Stop();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error..... " + e.StackTrace);
+                }
+            }
+        }
+    }
+}
