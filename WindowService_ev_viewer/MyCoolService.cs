622e2d7dc7ba981b8d71e7f8816e5f3f0a66f5e8
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2020-03-06 16:35:27 +0200

    Add files via upload

---
+++ WindowService_ev_viewer/MyCoolService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyCoolService
+{
+    public partial class MyCoolService : ServiceBase
+    {
+        public MyCoolService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                eventLog.Source = "Application";
+                eventLog.WriteEntry("My cool service started ...", EventLogEntryType.Information, 101, 1);
+            }
+}
+        }
+
+        protected override void OnStop()
+        {
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                eventLog.Source = "Application";
+                eventLog.WriteEntry("My cool service finished...", EventLogEntryType.Information, 101, 1);
+            }
+        }
+    }
+}
+
---
+++ WindowService_ev_viewer/ProjectInstaller.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MyCoolService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mycoolserviceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.mycoolserviceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // mycoolserviceProcessInstaller
+            // 
+            this.mycoolserviceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.mycoolserviceProcessInstaller.Password = null;
+            this.mycoolserviceProcessInstaller.Username = null;
+            // 
+            // mycoolserviceInstaller
+            // 
+            this.mycoolserviceInstaller.Description = "hi";
+            this.mycoolserviceInstaller.ServiceName = "MyCoolService";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.mycoolserviceProcessInstaller,
+            this.mycoolserviceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller mycoolserviceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller mycoolserviceInstaller;
+    }
+}
